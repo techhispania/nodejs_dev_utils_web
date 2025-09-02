f8bc319469a4cb9956ddeb8b301542b3a302b76d
enrique.sanchez <enrique.sanchez@hpe.com>
2025-09-02 13:35:12 +0200

    feat: json beautifier working

--- server.js
+++ server.js
@@ -9,10 +9,15 @@ app.engine("handlebars", engine())
 app.set("view engine", "handlebars")
 app.set("views", "./views")
 
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
     res.render("index", {
-        title: TITLE,
-        body_title: "JSON Beautifier"
+        title: TITLE
+    })
+})
+
+app.get("/json-beautifier", (req, res) => {
+    res.render("json-beautifier", {
+        title: `${TITLE} - JSON Beautifier`
     })
 })
 
