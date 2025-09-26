e9a661ca8bdc5c55f46ab38f68743f8cde50a4de
Enrique Sanchez <esanchez.devel@gmail.com>
2025-09-26 14:02:25 +0200

    feat: add button to show credentials and await for mongo connection

--- infrastructure/db/mongo-db.js
+++ infrastructure/db/mongo-db.js
@@ -4,15 +4,21 @@ const logger = require("../../application/logger")
 async function connect() {
     const uri = `mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_SERVER}:${process.env.MONGO_PORT}/${process.env.MONGO_DB}?authSource=admin`;
 
-    mongoose.connect(uri)
-    .then(() => logger.debug("MongoDB connected"))
-    .catch(err => logger.error("Connection error:", err));
+    try {
+        await mongoose.connect(uri)
+        logger.debug("MongoDB connected")
+    } catch (err) {
+        logger.error("Connection error:", err)
+    }
 }
 
 async function disconnect() {
-    mongoose.disconnect()
-    .then(() => logger.debug("MongoDB disconnected"))
-    .catch(err => logger.error("Error disconnecting from MongoDB:", err))
+    try {
+        await mongoose.disconnect()
+        logger.debug("MongoDB disconnected")
+    } catch (err) {
+        logger.error("Error disconnecting from MongoDB:", err)
+    }
 }
 
 module.exports = { connect, disconnect }
